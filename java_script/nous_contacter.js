ac3c63e435b3b673aca96d09e4160907bbeb180e
UnBretonFou <119815602+UnBretonFou@users.noreply.github.com>
2023-01-04 22:41:56 +0100

    ajout nousç_contacter.js

--- java_script/nous_contacter.js
+++ java_script/nous_contacter.js
@@ -4,4 +4,16 @@ const surnameField = document.getElementById('nom')
 const nameField = document.getElementById('prenom')
 const emailField = document.getElementById('email')
 const phoneField = document.getElementById('telephone')
-const messageFied = document.getElementById('motif')
+const messageField = document.getElementById('motif')
+
+// Fonction de validation du formulaire
+function validateForm() {
+    // Réinitialisation des erreurs
+    surnameField.setCustomValidity('');
+    nameField.setCustomValidity('');
+    emailField.setCustomValidity('');
+    phoneField.setCustomValidity('');
+    messageField.setCustomValidity('');
+}
+
+// Vérification du nom 
