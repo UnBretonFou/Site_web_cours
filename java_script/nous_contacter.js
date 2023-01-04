40c6c5f012c013120526a71e0df3a7fc52ecdc68
UnBretonFou <119815602+UnBretonFou@users.noreply.github.com>
2023-01-04 22:53:40 +0100

    modif rubrique (nous contacter)

--- java_script/nous_contacter.js
+++ java_script/nous_contacter.js
@@ -17,3 +17,25 @@ function validateForm() {
 }
 
 // Vérification du nom 
+if (surnameField.ariaValueMax.trim() === "") {
+    surnameField.sutCustomValidity("Veuillez saisir votre nom.");
+    return false;
+}
+
+// Vérification du prénom 
+if (nameField.ariaValueMax.trim() === "") {
+    nameField.sutCustomValidity("Veuillez saisir votre nom.");
+    return false;
+}
+
+// Vérification de l'email
+if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailField.value)) {
+    emailField.setCustomValidity("Veuillez saisir une adresse email valide.");
+    return false;
+}
+
+// Vérification du massage
+if (messageField.value.trim() === "") {
+    messageField.setCustomValidity("Veuillez saisir votre message.");
+    return false;
+}
