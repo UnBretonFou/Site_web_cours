de21879a76996e90b77aeb9399c8ea91da15998d
UnBretonFou <119815602+UnBretonFou@users.noreply.github.com>
2023-01-04 22:41:08 +0100

    MAJ du site

---
+++ java_script/nous_contacter.js
@@ -0,0 +1,7 @@
+// Récupération des champs du formulaire
+const form = document.getElementById('contact-form')
+const surnameField = document.getElementById('nom')
+const nameField = document.getElementById('prenom')
+const emailField = document.getElementById('email')
+const phoneField = document.getElementById('telephone')
+const messageFied = document.getElementById('motif')
--- java_script/parametre_main.js
+++
@@ -1,8 +0,0 @@
-const video =
-document.getElementById("video");
-video.play();
-video.addEventListener("ended"), () => {
-    video.src="../video/cocktail_2.mp4";
-    video.onload();
-    video.play()
-}
