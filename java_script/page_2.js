6f68a5d50a503d69b7a39041a2d1b2517fb866cb
UnBretonFou <119815602+UnBretonFou@users.noreply.github.com>
2023-01-23 13:51:15 +0100

    connexion api

--- java_script/page_2.js
+++ java_script/page_2.js
@@ -7,22 +7,47 @@ function closeForm() {
 }
 
 
+/* --- COKTAIL PAR SON NOM --- */
+function getRandomCocktail() {
+  fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s')
+  .then(response => response.json())
+  .then(data => {
+    // Extraire les infos de l'API
+    const cocktail = data.drinks[0];
+    console.log(data);
+    const name = cocktail.strDrink;
+    const instructions = cocktail.strInstructions;
+    const imageURL = cocktail.strDrinkThumb;
+
+    // Création de la nouvelle fenêtre
+    let newWindow = window.open("", "Cocktail aléatoire", "height=500", "widht=500");
+
+    // Ajout des informations dans la fenêtre
+    newWindow.document.write('<h1>' + name + '</h1>');
+    newWindow.document.write('<img src="' + imageURL + '" alt="' + name + '">');
+    newWindow.document.write('<p>' + instructions + '</p>')
+  });
+}
+
 /* --- COKTAIL ALEATOIRE --- */
-function openCocktailPopup() {
-    // Ouvre une fenêtre pop-up
-    const popup = window.open("", "Cocktail aléatoire", "height=500,width=500");
-    
-    // Effectue la requête API
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-      .then(response => response.json())
-      .then(data => {
-        // Met à jour le contenu de la fenêtre pop-up avec les données de cocktail aléatoire
-        popup.document.body.innerHTML = `
-          <h1>${data.strDrink}</h1>
-          <img src="${data.strDrinkThumb}" alt="${data.strDrink}">
-          <p>Ingrédients: ${data.strIngredient1}, ${data.strIngredient2}, ...</p>
-          <p>Instructions: ${data.strInstructions}</p>
-        `;
-      })
-      .catch(error => console.error(error))
-  }
+function getRandomCocktail() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then(response => response.json())
+  .then(data => {
+    // Extraire les infos de l'API
+    const cocktail = data.drinks[0];
+    console.log(data);
+    const name = cocktail.strDrink;
+    const instructions = cocktail.strInstructions;
+    const imageURL = cocktail.strDrinkThumb;
+
+    // Création de la nouvelle fenêtre
+    let newWindow = window.open("", "Cocktail aléatoire", "height=500", "widht=500");
+
+    // Ajout des informations dans la fenêtre
+    newWindow.document.write('<h1>' + name + '</h1>');
+    newWindow.document.write('<img src="' + imageURL + '" alt="' + name + '">');
+    newWindow.document.write('<p>' + instructions + '</p>')
+  });
+}
+
